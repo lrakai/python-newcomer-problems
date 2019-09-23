f6d414ee1bfd54fbd82a3de756ad642226aa48be
lrakai <lmrakai@gmail.com>
2019-09-23 13:54:49 -0600

    Add challenge two (pass by ref)

--- test/test_challenges.py
+++ test/test_challenges.py
@@ -4,6 +4,7 @@ import unittest
 
 sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../src")
 from challenge_one import string_fun
+from challenge_two import append
 
 class TestChallenge(unittest.TestCase):
 
@@ -13,6 +14,9 @@ class TestChallenge(unittest.TestCase):
         self.assertEqual(result[1], endswith_exclam)
         self.assertEqual(result[2], replace_spaces)
 
+    def _check_append(self, l, item, solution):
+        append(l, item)
+        self.assertEqual(l, solution)
 
     def test_challenge_one(self):
         self._check_string_fun(string_fun('Hello World!'), False, True, 'Hello-World!')
@@ -20,7 +24,8 @@ class TestChallenge(unittest.TestCase):
         self._check_string_fun(string_fun('    '), False, False, '----')
 
     def test_challenge_two(self):
-        pass
+        self._check_append([1, 2, 3], 4, [1, 2, 3, 4])
+        self._check_append(['a','b', 'c'], 'd', ['a','b','c','d'])
 
     def test_challenge_three(self):
         pass
